ea3d813c394100fd1cdcaea13fdd0078c7ad3134
JosMiguelMMGlocation <miguel.mendez@glocation.com.co>
2026-05-15 15:28:08 -0500

    Update Glocation email address for spam reports
    Change the complaint email address in `sendMailWithAttachment.js` from
    opensignlabs.com to glocation.com.co. This ensures that spam reports are
    directed to the correct support team.

--- url_firma.js
+++ url_firma.js
@@ -1,4 +1,4 @@
 // ID_DOCUMENTO / CORREO / ID_FIRMANTE
-const urlString = "MS0fE17bHG/miguel.mendez@glocation.com.co/jd70GvakGz";
+const urlString = "lfxQFk1hcu/miguel.mendez@glocation.com.co/jd70GvakGz";
 const base64Code = Buffer.from(urlString).toString('base64');
 console.log(base64Code);
