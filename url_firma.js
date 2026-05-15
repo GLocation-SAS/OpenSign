30a12e29b5627c8b334134d2c3629a274755c715
JosMiguelMMGlocation <miguel.mendez@glocation.com.co>
2026-05-15 13:43:06 -0500

    Update app name and URL string
    Replaces "OpenSign™" with "Glocation Sign" in completion email
    configuration. Also updates a base64 encoded URL string.

--- url_firma.js
+++ url_firma.js
@@ -1,4 +1,4 @@
 // ID_DOCUMENTO / CORREO / ID_FIRMANTE
-const urlString = "CMlIic8RBl/miguel.mendez@glocation.com.co/jd70GvakGz";
+const urlString = "SDv979o6Yf/miguel.mendez@glocation.com.co/jd70GvakGz";
 const base64Code = Buffer.from(urlString).toString('base64');
 console.log(base64Code);
