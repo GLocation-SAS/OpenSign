28376425bcc393cf8bb128b6affe428f90bac7fa
JosMiguelMMGlocation <miguel.mendez@glocation.com.co>
2026-05-20 11:26:35 -0500

    Update base64 encoded URL string

--- url_firma.js
+++ url_firma.js
@@ -1,4 +1,4 @@
 // ID_DOCUMENTO / CORREO / ID_FIRMANTE
-const urlString = "R7jkdWQT7g/miguel.mendez@glocation.com.co/jd70GvakGz";
+const urlString = "EL7UUF0V1I/miguel.mendez@glocation.com.co/jd70GvakGz";
 const base64Code = Buffer.from(urlString).toString('base64');
 console.log(base64Code);
